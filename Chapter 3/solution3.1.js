e564d8dbcc79416d01d37378edfa80618f1ba051
Nana Owusu Ansah <67644108+tiptipd@users.noreply.github.com>
2023-01-19 17:49:54 +0500

    Solution 3.1

---
+++ Chapter 3/solution3.1.js
@@ -0,0 +1,16 @@
+function FindMin(var1, var2) {
+  if (var2 < var1) 
+	  return var2;
+  else if (var1 < var1) 
+	  return var1;
+  else
+  {
+	  return "both numbers are equal";
+  }
+}
+
+let v1 = 100, v2 = 20;
+console.log(FindMin(v1, v2));
+// → 0
+console.log(FindMin(v2, v2));
+// → -10

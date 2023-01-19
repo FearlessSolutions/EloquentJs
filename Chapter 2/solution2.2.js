f137bd45a4e9af30ca3fbd03c8ebf52271096fc3
Nana Owusu Ansah <67644108+tiptipd@users.noreply.github.com>
2023-01-19 17:52:44 +0500

    solution2.2_2.3

---
+++ Chapter 2/solution2.2.js
@@ -0,0 +1,13 @@
+let i = 1;
+while (i <= 100) {
+    let print = "";
+  let output = "";
+  if (i % 3 == 0) 
+      print = print + "Fizz"
+  if (i % 5 == 0)
+	  print = print + "Buzz";
+  if (i % 3 != 0 && i % 5 != 0) 
+	  print = print + i
+  i++
+  console.log (print);
+}
---
+++ Chapter 2/solution2.3.js
@@ -0,0 +1,35 @@
+let size = 8;
+
+let board = "";
+let i = 0;
+let flag = true;
+while (i < size) 
+{
+	let j = 0;
+	if (i % 2 == 0)
+	{board += " ";}
+	
+	while (j < size) 
+	{
+		if (flag == true) 
+		{
+			board += " ";
+			flag = false;
+		} 
+		else 
+		{
+			board += "#";
+			flag = true;
+		}
+		
+		j++;
+		
+	}
+	if (flag == true)
+	{flag = false;}
+
+  board += "\n";
+  i++;
+}
+
+console.log(" "+board);
